2f16fe7d2d770a050dbfed0011d3b79748da0acc
Mason Thomas <Kandleseatwicks@gmail.com>
2023-10-05 16:32:21 -0500

    Fix inverted coords issue, create printBoard function, update empty condition.

--- webServer/test.js
+++ webServer/test.js
@@ -5,17 +5,17 @@ const {BotManager, Point, ChessPiece, Path} = require('./include/BotManager');
 testBotManager = new BotManager();
 
 // output current status of chess board
-console.table(testBotManager.board);
+testBotManager.printBoard();
 
 // set test case of board
-testBotManager.board[1][1] = new ChessPiece(0, 'A', true, new Point(1, 1));
-testBotManager.board[2][1] = new ChessPiece(0, 'B', true, new Point(1, 2));
-testBotManager.board[3][1] = new ChessPiece(0, 'C', true, new Point(1, 3));
-testBotManager.board[3][2] = new ChessPiece(0, 'D', true, new Point(2, 3));
-testBotManager.board[3][3] = new ChessPiece(0, 'E', true, new Point(3, 3));
+testBotManager.board[1][1] = new ChessPiece(1, 'A', true, new Point(1, 1));
+testBotManager.board[1][2] = new ChessPiece(2, 'B', true, new Point(1, 2));
+testBotManager.board[1][3] = new ChessPiece(3, 'C', true, new Point(1, 3));
+testBotManager.board[2][3] = new ChessPiece(4, 'D', true, new Point(2, 3));
+testBotManager.board[3][3] = new ChessPiece(5, 'E', true, new Point(3, 3));
 
 // output current status of chess board
-console.table(testBotManager.board);
+testBotManager.printBoard();
 
 // TEST L PATTERN!
 console.log('L PATTERN:');
