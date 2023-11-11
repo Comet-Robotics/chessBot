0bebc4adeeb20be38285b72b9f90167f87fc6b4e
Alex Kempen <alex.bookreader@gmail.com>
2023-11-10 20:00:49 -0600

    Added vite starter code

---
+++ src/server/main.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import ViteExpress from "vite-express";
+
+const app = express();
+
+app.get("/hello", (_, res) => {
+  res.send("Hello Vite + React + TypeScript!");
+});
+
+// Use an express router to redirect /api to backend/api
+
+ViteExpress.listen(app, 3000, () =>
+  console.log("Server is listening on port 3000...")
+);
