83829764c446dabdf1181f983d0bbc7f2b6e37c8
Alex Kempen <alex.bookreader@gmail.com>
2023-11-28 20:10:35 -0600

    Modified cookie to be passive middleware on all requests

--- src/server/api.ts
+++ src/server/api.ts
@@ -5,27 +5,10 @@ import { CommandExecutor } from "./command/executor";
 import { Router } from "express";
 import { Square } from "./robot/square";
 
-import { v4 as uuid } from "uuid";
-
 export const router = Router();
 
 const manager = new PieceManager([]);
 const executor = new CommandExecutor();
-
-router.get("/auth", (req, res) => {
-  //pulls clientId cookie from client
-  const clientIdCookie = req.cookies.clientId;
-
-  // if cookie exists on client, return the cookie
-  if (clientIdCookie) {
-    res.json({ clientId: clientIdCookie });
-  } else {
-    const newId = uuid();
-    //sets cookie to expire after 1 day from generation
-    res.cookie("clientId", newId, { maxAge: 86400000 });
-    res.json({ clientId: newId });
-  }
-});
 
 router.post("/reset", (req, res) => {
   reset();
@@ -43,6 +26,8 @@ router.post("/move", (req, res) => {
   } else if (!req.query.start || !req.query.end) {
     return res.status(400).json({ message: "'start' and 'end' are required" });
   }
+
+  console.log(req.cookies.clientId);
 
   console.log("Start: " + req.query.start);
   console.log("End: " + req.query.end);
