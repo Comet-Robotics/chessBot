1b5f4b58169cb9271795309a2047ce00c99e38b2
Alex Kempen <alex.bookreader@gmail.com>
2023-11-29 19:50:15 -0600

    Reorganized client files

--- src/client/chessboard-wrapper.tsx
+++ src/client/chessboard-wrapper.tsx
@@ -7,14 +7,18 @@ interface ChessboardWrapperProps {
     onMove: (from: Square, to: Square) => boolean;
 }
 
-export function ChessboardWrapper(props: ChessboardWrapperProps) {
+export function ChessboardWrapper(props: ChessboardWrapperProps): JSX.Element {
     const { chess, isWhite, onMove } = props;
-    return (<Chessboard
-        boardOrientation={isWhite ? "white" : "black"}
-        boardWidth={500}
-        position={chess.fen()}
-        onPieceDrop={onMove}
-        isDraggablePiece={({ sourceSquare }) => chess.get(sourceSquare).color == (isWhite ? "w" : "b")}
-        arePremovesAllowed={true}
-    />);
+    return (
+        <div className="chess-container">
+            <Chessboard
+                boardOrientation={isWhite ? "white" : "black"}
+                boardWidth={500}
+                position={chess.fen()}
+                onPieceDrop={onMove}
+                isDraggablePiece={({ sourceSquare }) => chess.get(sourceSquare).color == (isWhite ? "w" : "b")}
+                arePremovesAllowed={true}
+            />
+        </div>
+    );
 }
