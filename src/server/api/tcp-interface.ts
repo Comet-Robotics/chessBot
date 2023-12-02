5dbe2538bc97e8d7e1506314d9cb0d732e6241bd
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2023-12-02 02:36:06 -0600

    Implemented tcp-interface

---
+++ src/server/api/tcp-interface.ts
@@ -0,0 +1,163 @@
+import * as net from "net";
+const config = require('../utils/config.json');
+
+// MUST be kept in sync with chessBotArduino/include/packet.h PacketType
+enum PacketType {
+    NOTHING,
+    CLIENT_HELLO,
+    SERVER_HELLO,
+    PING_SEND,
+    PING_RESPONSE,
+    QUERY_VAR,
+    QUERY_RESPONSE,
+    INFORM_VAR,
+    SET_VAR,
+    MOVE_TO_SPACE,
+    MOVE_TO_POS,
+    DRIVE
+}
+
+class BotTunnel {
+    connected: boolean = false;
+    dataBuffer: Buffer | undefined;
+    address: string | undefined;
+    id: number | undefined;
+
+    constructor(private socket: net.Socket, private onHandshake: (packetContent: string) => void) {
+        
+    }
+
+    isActive() {
+        return (this.socket.readyState === 'open');
+    }
+
+    getIdentifier() {
+        if (this.id !== undefined) {
+            return "ID: " + this.id;
+        } else if (this.address !== undefined) {
+            return "MAC Address: " + this.address;
+        } else if (this.socket.remoteAddress !== undefined) {
+            return "IP: " + this.socket.remoteAddress;
+        } else {
+            return 'Unnamed Robot';
+        }
+    }
+
+    onData(data: Buffer) {
+        console.log('connection data from %s: %j', this.getIdentifier(), data.toString());
+        this.handleData(data);
+    }
+
+    onError(err: Error) {
+        console.log('Connection error from %s: %s', this.getIdentifier, err);
+        this.connected = false;
+    }
+
+    onClose() {
+        console.log('Lost connection to %s', this.getIdentifier);
+        this.connected = false;
+    }
+
+    handleData(data: Buffer) {
+        if (this.dataBuffer !== undefined) {
+            this.dataBuffer = Buffer.concat([this.dataBuffer, data]);
+        } else {
+            this.dataBuffer = data;
+        }
+
+        this.handleQueue();
+    }
+
+    handleQueue() {
+        if (this.dataBuffer === undefined || this.dataBuffer.length < 3) {
+            this.dataBuffer = undefined;
+            return;
+        }
+
+        let str = this.dataBuffer.toString();
+        const terminator = str.indexOf(';');
+
+        if (terminator == -1) {
+            if (str.length > 200) {
+                // Invalid state, reset buf
+                this.dataBuffer = undefined;
+            }
+
+            // Continue waiting for rest of packet
+            return;
+        }
+
+        if (str.at(0) !== ':') {
+            this.dataBuffer = undefined;
+            return;
+        }
+
+        str = str.substring(1, terminator);
+
+        if (this.dataBuffer.length > terminator) {
+            this.dataBuffer = this.dataBuffer.subarray(terminator + 1);
+        } else {
+            this.dataBuffer = undefined;
+        }
+
+        const type = parseInt(str.substring(0, 2), 16);
+        const contents = str.substring(3);
+
+        this.handlePacket(type, contents);
+
+        if (this.dataBuffer !== undefined && this.dataBuffer.indexOf(';') != -1) {
+            this.handleQueue();
+        }
+    }
+
+    handlePacket(type: PacketType, contents: string) {
+        switch (type) {
+        case PacketType.NOTHING: {break;}
+        case PacketType.CLIENT_HELLO: {
+            this.onHandshake(contents);
+            this.connected = true;
+        }
+        }
+    }
+
+    send(contents: string) {
+        if (this.isActive()) {
+            this.socket.write(contents);
+        } else {
+            console.log('Connection to', this.getIdentifier(), 'is inactive, failed to write', contents);
+        }
+    }
+}
+
+class TCPServer {
+    connections: { [address: string] : BotTunnel} = {}; // Map of ID to BotConnection
+    server: net.Server;
+
+    constructor() {
+        this.server = net.createServer();
+        this.server.on('connection', this.handleConnection);
+        this.server.listen(3001, () => {
+            console.log('TCP bot server listening to %j',
+                this.server.address());
+        });
+    }
+
+    handleConnection(socket: net.Socket) {
+        const remoteAddress = socket.remoteAddress + ':' + socket.remotePort;
+        console.log('New client connection from %s', remoteAddress);
+
+        const connectionsReference = this.connections;
+        const tunnel = new BotTunnel(socket, (mac: string) => {
+            console.log('Adding robot with mac', mac, 'to arr');
+            connectionsReference[config['bots'][mac]] = tunnel;
+        });
+
+        socket.on('data', tunnel.onData);
+        socket.once('close', tunnel.onClose);
+        socket.on('error', tunnel.onError);
+    };
+
+    getBotAddressFromID(id: number) {
+        return this.connections[id.toString()];
+    }
+}
