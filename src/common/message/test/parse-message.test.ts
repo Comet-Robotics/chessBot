8827663027db7eabf01bea4f4b05df442f707245
Alex Kempen <alex.bookreader@gmail.com>
2024-02-12 22:07:09 -0600

    Fixed formatting

--- src/common/message/test/parse-message.test.ts
+++ src/common/message/test/parse-message.test.ts
@@ -1,6 +1,7 @@
 import { StopGameReason } from "../../game-end";
 import { GameType } from "../../game-type";
 import { StartGameMessage, StopGameMessage } from "../game-message";
+import { Message } from "../message";
 import { parseMessage } from "../parse-message";
 import { expect, test } from "vitest";
 
@@ -8,7 +9,7 @@ test.each([
     new StartGameMessage(GameType.COMPUTER, 3),
     new StartGameMessage(GameType.HUMAN),
     new StopGameMessage(StopGameReason.ABORTED),
-])("Message should serialize correctly", (message) => {
+])("Message should serialize correctly", (message: Message) => {
     const copy = Object.assign({}, message);
     expect(parseMessage(message.toJson())).toEqual(copy);
 });
