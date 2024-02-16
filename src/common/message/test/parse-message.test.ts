840fc2125e5e5dcd21c840eb60afaf476dbbf604
Alex Kempen <alex.bookreader@gmail.com>
2024-02-16 17:19:14 -0600

    Added message test cases

--- src/common/message/test/parse-message.test.ts
+++ src/common/message/test/parse-message.test.ts
@@ -1,6 +1,12 @@
 import { StopGameReason } from "../../game-end";
 import { GameType } from "../../game-type";
-import { StartGameMessage, StopGameMessage } from "../game-message";
+import { DriveRobotMessage, StopRobotMessage } from "../drive-robot-message";
+import {
+    MoveMessage,
+    PositionMessage,
+    StartGameMessage,
+    StopGameMessage,
+} from "../game-message";
 import { Message } from "../message";
 import { parseMessage } from "../parse-message";
 import { expect, test } from "vitest";
@@ -9,6 +15,10 @@ test.each([
     new StartGameMessage(GameType.COMPUTER, 3),
     new StartGameMessage(GameType.HUMAN),
     new StopGameMessage(StopGameReason.ABORTED),
+    new PositionMessage("aaaaaaaaa"),
+    new MoveMessage("a1", "a4"),
+    new DriveRobotMessage("robot1", 0.5, 0.5),
+    new StopRobotMessage("robot2"),
 ])("Message should serialize correctly", (message: Message) => {
     const copy = Object.assign({}, message);
     expect(parseMessage(message.toJson())).toEqual(copy);
