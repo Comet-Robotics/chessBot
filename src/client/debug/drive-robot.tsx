547699b5978b1a15a4a3b756ccb600a21db3f470
MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
2024-02-17 00:37:51 -0600

    Wrapped blueprintprovider , implemented hotkeys feature in drive-robot.

--- src/client/debug/drive-robot.tsx
+++ src/client/debug/drive-robot.tsx
@@ -1,13 +1,14 @@
-import { Button } from "@blueprintjs/core";
+import { Button , useHotkeys} from "@blueprintjs/core";
+import React, { createRef, useCallback, useMemo} from "react";
 import { SendMessage } from "react-use-websocket";
 import { StopRobotMessage } from "../../common/message/drive-robot-message";
 import { DriveRobotMessage } from "../../common/message/drive-robot-message";
+
 
 interface DriveRobotProps {
     robotId: string;
     sendMessage: SendMessage;
 }
-
 /**
  * A component which can be used to drive an individual robot around.
  */
@@ -32,8 +33,88 @@ export function DriveRobot(props: DriveRobotProps) {
         return handleManualMove;
     };
 
+    const handleDriveForward = getManualMoveHandler(1, 1);
+    const handleDriveBackward = getManualMoveHandler(-1, -1);
+    const handleDriveRight = getManualMoveHandler(0.5, -0.5);
+    const handleDriveLeft = getManualMoveHandler(-0.5, 0.5);
+
+/* Declare other handlers here */
+
+const hotkeys = useMemo(
+      () => [
+          {
+              combo: "w",
+              group: "Debug",
+              global: true, 
+              label: "Drive forward",
+              onKeyDown: handleDriveForward,
+             onKeyUp: handleStopMove
+          },
+          {
+            combo: "s",
+            group: "Debug",
+            global: true, 
+            label: "Drive backward",
+            onKeyDown: handleDriveBackward,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "a",
+            group: "Debug",
+            global: true, 
+            label: "Drive Right",
+            onKeyDown: handleDriveRight,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "d",
+            group: "Debug",
+            global: true, 
+            label: "Drive left",
+            onKeyDown: handleDriveLeft,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "up",
+            group: "Debug",
+            global: true, 
+            label: "Drive forward",
+            onKeyDown: handleDriveForward,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "down",
+            group: "Debug",
+            global: true, 
+            label: "Drive backward",
+            onKeyDown: handleDriveBackward,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "right",
+            group: "Debug",
+            global: true, 
+            label: "Drive Right",
+            onKeyDown: handleDriveRight,
+           onKeyUp: handleStopMove
+        },
+        {
+            combo: "left",
+            group: "Debug",
+            global: true, 
+            label: "Drive left",
+            onKeyDown: handleDriveLeft,
+           onKeyUp: handleStopMove
+        },
+
+          // ... declare other key combos for wasd and arrow keys (see documentation)
+      ],
+      [handleStopMove, handleDriveForward, handleDriveBackward , handleDriveLeft , handleDriveRight],
+  );
+
+  const{ handleKeyDown, handleKeyUp } = useHotkeys(hotkeys); 
     return (
-        <>
+        <div tabIndex={0} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}>
             <Button
                 icon="arrow-up"
                 onMouseUp={handleStopMove}
@@ -55,6 +136,7 @@ export function DriveRobot(props: DriveRobotProps) {
                 onMouseUp={handleStopMove}
                 onMouseDown={getManualMoveHandler(0.5, -0.5)}
             />
-        </>
+        </div>
+    
     );
 }
