a4307d4f805326968bfb4e5efbdad21cb7d69fb8
Colin Wong <cwongmath@gmail.com>
2024-02-17 00:44:43 -0600

    Fix build error (#112)
    Also adds PromotionMessage test

--- src/common/message/test/parse-message.test.ts
+++ src/common/message/test/parse-message.test.ts
@@ -1,22 +1,15 @@
 import { StopGameReason } from "../../game-end";
 import { GameType } from "../../game-type";
+import { PieceType } from "../../types";
 import { DriveRobotMessage, StopRobotMessage } from "../drive-robot-message";
 import {
     MoveMessage,
     PositionMessage,
+    PromotionMessage,
     StartGameMessage,
     StopGameMessage,
 } from "../game-message";
 import { Message } from "../message";
-import { PieceType } from "../../types";
-import {
-    StartGameMessage,
-    StopGameMessage,
-    PromotionMessage,
-    PositionMessage,
-    MoveMessage,
-} from "../game-message";
-import { DriveRobotMessage, StopRobotMessage } from "../drive-robot-message";
 import { parseMessage } from "../parse-message";
 import { expect, test } from "vitest";
 
@@ -25,6 +18,7 @@ test.each([
     new StartGameMessage(GameType.HUMAN, false),
     new StopGameMessage(StopGameReason.ABORTED),
     new PositionMessage("aaaaaaaaa"),
+    new PromotionMessage("h7", "b3", PieceType.KNIGHT),
     new MoveMessage("a1", "a4"),
     new DriveRobotMessage("robot1", 0.5, 0.5),
     new StopRobotMessage("robot2"),
