3629a2605e41bc47daa40cece80baa035d4011dd
Alex Kempen <alex.bookreader@gmail.com>
2024-02-26 20:51:25 -0600

    Fixed minor type issues

--- src/client/game/game.tsx
+++ src/client/game/game.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useEffect, useState } from "react";
+import { Dispatch, ReactElement, useEffect, useState } from "react";
 import { Square } from "chess.js";
 
 import {
@@ -54,7 +54,7 @@ export function Game(): JSX.Element {
         sendMessage(new StartGameMessage(gameType, side, difficulty));
     }, [sendMessage, gameType, side, difficulty]);
 
-    let gameEndDialog = null;
+    let gameEndDialog: ReactElement | null = null;
     if (chess.isGameOver()) {
         gameEndDialog = (
             <GameEndDialog
