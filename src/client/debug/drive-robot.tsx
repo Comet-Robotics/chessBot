0e5ed070896731e489107c4c9c067e57df4ddb7f
Alex Kempen <89172296+AlexKempen@users.noreply.github.com>
2024-02-26 20:56:28 -0600

    80 client add hotkeys to debug menu (#119)
    * Wrapped blueprintprovider , implemented hotkeys feature in drive-robot.
    
    * updated formatting, updated packages
    
    * Renamed drive to turn and added additional handler useage
    
    * Minor spelling
    
    ---------
    
    Co-authored-by: MuditUpadhyay04 <mudit.upadhyay04@gmail.com>
    Co-authored-by: harsh2226 <hmp210003@utdallas.edu>

--- src/client/debug/drive-robot.tsx
+++ src/client/debug/drive-robot.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@blueprintjs/core";
+import { Button, useHotkeys } from "@blueprintjs/core";
+import { useMemo } from "react";
 import { StopRobotMessage } from "../../common/message/drive-robot-message";
 import { DriveRobotMessage } from "../../common/message/drive-robot-message";
 import { SendMessage } from "../../common/message/message";
@@ -7,7 +8,6 @@ interface DriveRobotProps {
     robotId: string;
     sendMessage: SendMessage;
 }
-
 /**
  * A component which can be used to drive an individual robot around.
  */
@@ -20,37 +20,117 @@ export function DriveRobot(props: DriveRobotProps) {
         leftPower: number,
         rightPower: number,
     ): (() => void) => {
-        const handleManualMove = () => {
+        const handleManualMove = () =>
             props.sendMessage(
                 new DriveRobotMessage(props.robotId, leftPower, rightPower),
             );
-        };
         return handleManualMove;
     };
+    const handleDriveForward = getManualMoveHandler(1, 1);
+    const handleDriveBackward = getManualMoveHandler(-1, -1);
+    const handleTurnRight = getManualMoveHandler(0.5, -0.5);
+    const handleTurnLeft = getManualMoveHandler(-0.5, 0.5);
 
+    const hotkeys = useMemo(
+        () => [
+            {
+                combo: "w",
+                group: "Debug",
+                global: true,
+                label: "Drive forward",
+                onKeyDown: handleDriveForward,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "s",
+                group: "Debug",
+                global: true,
+                label: "Drive backward",
+                onKeyDown: handleDriveBackward,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "a",
+                group: "Debug",
+                global: true,
+                label: "Turn right",
+                onKeyDown: handleTurnRight,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "d",
+                group: "Debug",
+                global: true,
+                label: "Turn left",
+                onKeyDown: handleTurnLeft,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "up",
+                group: "Debug",
+                global: true,
+                label: "Drive forward",
+                onKeyDown: handleDriveForward,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "down",
+                group: "Debug",
+                global: true,
+                label: "Drive backward",
+                onKeyDown: handleDriveBackward,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "right",
+                group: "Debug",
+                global: true,
+                label: "Turn right",
+                onKeyDown: handleTurnRight,
+                onKeyUp: handleStopMove,
+            },
+            {
+                combo: "left",
+                group: "Debug",
+                global: true,
+                label: "Turn left",
+                onKeyDown: handleTurnLeft,
+                onKeyUp: handleStopMove,
+            },
+        ],
+        [
+            handleStopMove,
+            handleDriveForward,
+            handleDriveBackward,
+            handleTurnLeft,
+            handleTurnRight,
+        ],
+    );
+
+    const { handleKeyDown, handleKeyUp } = useHotkeys(hotkeys);
     return (
-        <>
+        <div tabIndex={0} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}>
             <Button
                 icon="arrow-up"
                 onMouseUp={handleStopMove}
-                onMouseDown={getManualMoveHandler(1, 1)}
+                onMouseDown={handleDriveForward}
             />
             <br />
             <Button
                 icon="arrow-left"
                 onMouseUp={handleStopMove}
-                onMouseDown={getManualMoveHandler(-0.5, 0.5)}
+                onMouseDown={handleTurnLeft}
             />
             <Button
                 icon="arrow-down"
                 onMouseUp={handleStopMove}
-                onMouseDown={getManualMoveHandler(-1, -1)}
+                onMouseDown={handleDriveBackward}
             />
             <Button
                 icon="arrow-right"
                 onMouseUp={handleStopMove}
-                onMouseDown={getManualMoveHandler(0.5, -0.5)}
+                onMouseDown={handleTurnRight}
             />
-        </>
+        </div>
     );
 }
