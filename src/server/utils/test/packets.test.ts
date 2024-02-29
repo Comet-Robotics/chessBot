3502bc537336c7d066e451d341ec10d79851b1d4
Colin Wong <cwongmath@gmail.com>
2024-02-28 20:48:38 -0600

    Fix test

--- src/server/utils/test/packets.test.ts
+++ src/server/utils/test/packets.test.ts
@@ -3,57 +3,57 @@ import { BotTunnel } from "../../api/tcp-interface";
 import { vi, test, expect, afterEach } from "vitest";
 import { Packet, packetToJson } from "../tcp-packet";
 
-vi.mock("../../api/tcp-interface");
+vi.mock("net");
 
 const mockSocket = vi.mocked(net.Socket.prototype);
-// mockSocket.readyState = "open"; // fucking hell it wont let me overwrite properties even on a mocked object
-// vi.spyOn(mockSocket, 'readyState', 'get').mockReturnValue('open');
-// Object.defineProperty(mockSocket, 'readyState', {value: 'open', writable: true})
 
 const mockBotTunnel = new BotTunnel(mockSocket, (addr) => {
     console.log(addr);
 });
-
-// vi.spyOn(console, 'log').withImplementation((msg) => console.log(msg));
-
 mockBotTunnel.id = 69;
 mockBotTunnel.address = "lul";
 
 const mockWrite = vi.spyOn(mockSocket, "write");
 
+const testCases = [
+    [{}, false],
+    [{ type: "NOTHING" }, true],
+    [{ type: "CLIENT_HELLO" }, false],
+    [{ type: "CLIENT_HELLO", macAddress: "HELLO3" }, true],
+    [{ type: "DRIVE_TANK" }, false],
+    [{ type: "DRIVE_TANK", left: 0.3 }, false],
+    [{ type: "DRIVE_TANK", left: 0.3, right: -0.4 }, true],
+] as [object, boolean][];
+
 afterEach(() => {
     vi.clearAllMocks();
 });
 
-// test.each([
-//     [{}, false],
-//     [{ type: "NOTHING" }, true],
-//     [{ type: "CLIENT_HELLO" }, false],
-//     [{ type: "CLIENT_HELLO", macAddress: "HELLO3" }, true],
-// ])(
-//     "Packet guard",
-//     async (obj: object, guard_value: boolean) => {
-//         // Packet.
-//         expect(Packet.guard(obj)).toEqual(guard_value);
-//     }
-// )
+test.each(testCases)(
+    "Validate packet guard",
+    async (obj: object, valid: boolean) => {
+        expect(Packet.guard(obj)).toEqual(valid);
+    },
+);
 
-test.each([
-    [{}, null],
-    [{ type: "NOTHING" }, JSON.stringify({ type: "NOTHING" })],
-    [{ type: "CLIENT_HELLO" }, null],
-    [
-        { type: "CLIENT_HELLO", macAddress: "HELLO3" },
-        JSON.stringify({ type: "CLIENT_HELLO", macAddress: "HELLO3" }),
-    ],
-])("Test packet writing", async (packet: object, expected: string | null) => {
-    vi.spyOn(mockBotTunnel, "isActive").mockReturnValue(true);
-    expect(mockBotTunnel.isActive()).toEqual(true);
+test.each(testCases)(
+    "Verify packetToJson",
+    async (obj: object, valid: boolean) => {
+        expect(packetToJson(obj as Packet)).toEqual(
+            valid ? JSON.stringify(obj) : null,
+        );
+    },
+);
 
-    expect(packetToJson(packet as Packet)).toEqual(expected);
-
-    mockBotTunnel.send(packet as Packet);
-    if (expected !== null)
-        expect(mockWrite.mock.calls[0][0]).toStrictEqual(`:${expected};`);
-    else expect(mockWrite.mock.calls.length).toEqual(0);
-});
+test.each(testCases)(
+    "Test message sending",
+    async (obj: object, valid: boolean) => {
+        vi.spyOn(mockBotTunnel, "isActive").mockReturnValue(true);
+        mockBotTunnel.send(obj as Packet);
+        if (valid)
+            expect(mockWrite.mock.calls[0][0]).toStrictEqual(
+                `:${JSON.stringify(obj)};`,
+            );
+        else expect(mockWrite.mock.calls.length).toEqual(0);
+    },
+);
