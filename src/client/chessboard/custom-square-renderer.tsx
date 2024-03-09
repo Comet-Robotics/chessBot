b4213c8828b9686512303d6c1a54a664a3562ed9
Gabriel Burbach <gabriel.burbach02@gmail.com>
2024-03-09 01:26:42 -0600

    fixed lint errors added new files for svg components

--- src/client/chessboard/custom-square-renderer.tsx
+++ src/client/chessboard/custom-square-renderer.tsx
@@ -1,6 +1,7 @@
 import { CustomSquareProps } from "react-chessboard/dist/chessboard/types";
 import { ChessEngine } from "../../common/chess-engine";
 import { ReactElement, forwardRef } from "react";
+import { Circle, PieceSquare } from "./svg-components";
 
 export function getCustomSquareRenderer(
     legalSquares: string[],
@@ -36,50 +37,3 @@ export function getCustomSquareRenderer(
         );
     });
 }
-//SVG of the circle used to show legal moves
-const Circle = () => {
-    return (
-        <svg>
-            <circle cx="50%" cy="50%" r="15%" fill="green" />
-        </svg>
-    );
-};
-
-//SVG of the highlight used to show a legal capture
-const PieceSquare: React.FC<CustomSquareProps> = forwardRef((props, _ref) => {
-    return (
-        <div
-            style={{
-                position: "absolute",
-                height: props.style.height,
-                width: props.style.width,
-            }}
-        >
-            <svg
-                viewBox={"0 0 298 298"}
-                height={"100%"}
-                width={"100%"}
-                style={{
-                    position: "relative",
-                }}
-            >
-                <path
-                    d="M84.52,0H0v84.52C16.49,46.8,46.8,16.49,84.52,0Z"
-                    fill="green"
-                />
-                <path
-                    d="M300,84.52V0h-84.52c37.73,16.49,68.03,46.8,84.52,84.52Z"
-                    fill="green"
-                />
-                <path
-                    d="M0,215.48v84.52h84.52C46.8,283.51,16.49,253.2,0,215.48Z"
-                    fill="green"
-                />
-                <path
-                    d="M215.48,300h84.52v-84.52c-16.49,37.73-46.8,68.03-84.52,84.52Z"
-                    fill="green"
-                />
-            </svg>
-        </div>
-    );
-});
