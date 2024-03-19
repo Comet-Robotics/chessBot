89c166fec13f808c6c2ffb6a9c1536adb8c6764b
Gabriel Burbach <gabriel.burbach02@gmail.com>
2024-03-18 20:37:40 -0500

    Custom Square Render (#138)
    * added a custom square renderer for the custom square styles we made to show legal moves. This uses SVGs that we add to the react chessboard to show the new renders
    
    * removed comments
    
    * fixed lint errors added new files for svg components
    
    * turned the svg into components and removed forwardRef
    
    * brought back forwardRef :)

--- src/client/chessboard/chessboard-wrapper.tsx
+++ src/client/chessboard/chessboard-wrapper.tsx
@@ -5,10 +5,7 @@ import { BoardContainer } from "./board-container";
 import { ChessEngine } from "../../common/chess-engine";
 import { Move } from "../../common/game-types";
 import { Side, PieceType } from "../../common/game-types";
-
-const CLICK_STYLE = {
-    backgroundColor: "green",
-};
+import { getCustomSquareRenderer } from "./custom-square-renderer";
 
 interface ChessboardWrapperProps {
     /**
@@ -44,13 +41,9 @@ export function ChessboardWrapper(props: ChessboardWrapperProps): JSX.Element {
     >();
 
     // Maps squares to style objects
-    const customSquareStyles: { [square: string]: object } = {};
-    let legalSquares: string[] | undefined = undefined;
+    let legalSquares: string[] = [];
     if (lastClickedSquare !== undefined) {
         legalSquares = chess.getLegalSquares(lastClickedSquare);
-        legalSquares.forEach((square) => {
-            customSquareStyles[square] = CLICK_STYLE;
-        });
     }
 
     /**
@@ -117,9 +110,9 @@ export function ChessboardWrapper(props: ChessboardWrapperProps): JSX.Element {
                 onSquareClick={(square: Square) => {
                     setManualPromotionSquare(undefined);
 
+                    // Protects the type of lastClickedSquare, and is true when the clicked square is legal
                     const isSquareLegalMove =
                         lastClickedSquare !== undefined &&
-                        legalSquares !== undefined &&
                         legalSquares.includes(square);
 
                     if (isSquareLegalMove) {
@@ -146,7 +139,7 @@ export function ChessboardWrapper(props: ChessboardWrapperProps): JSX.Element {
                     return piece[0] === side;
                 }}
                 arePremovesAllowed={false}
-                customSquareStyles={customSquareStyles}
+                customSquare={getCustomSquareRenderer(legalSquares, chess)}
             />
         </BoardContainer>
     );
