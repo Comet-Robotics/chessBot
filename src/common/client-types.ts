08ce9dd9d951726761e58904397dd4235ad58a07
Colin Wong <cwongmath@gmail.com>
2024-04-09 03:07:01 -0500

    Merge game types

--- src/common/client-types.ts
+++ src/common/client-types.ts
@@ -1,18 +1,15 @@
-export enum GameType {
-    COMPUTER = "computer",
-    HUMAN = "human",
-} /**
+/**
  * Defines a type of session.
  */
-
 export enum ClientType {
     HOST = "host",
     CLIENT = "client",
     SPECTATOR = "spectate",
-} /*
+}
+
+/**
  * Difficulties available for AI.
  */
-
 export enum Difficulty {
     BABY = 0,
     BEGINNER = 1,
