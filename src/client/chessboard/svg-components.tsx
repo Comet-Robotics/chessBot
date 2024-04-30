531e465b2df6ec3c4038f9af5534d9218150b4f4
Mason Thomas <Kandleseatwicks@gmail.com>
2024-04-29 22:14:21 -0500

    update green hex code

--- src/client/chessboard/svg-components.tsx
+++ src/client/chessboard/svg-components.tsx
@@ -25,19 +25,19 @@ export function OutsideCorners(props: PieceSquareProps) {
             >
                 <path
                     d="M84.52,0H0v84.52C16.49,46.8,46.8,16.49,84.52,0Z"
-                    fill="green"
+                    fill="#275133"
                 />
                 <path
                     d="M300,84.52V0h-84.52c37.73,16.49,68.03,46.8,84.52,84.52Z"
-                    fill="green"
+                    fill="#275133"
                 />
                 <path
                     d="M0,215.48v84.52h84.52C46.8,283.51,16.49,253.2,0,215.48Z"
-                    fill="green"
+                    fill="#275133"
                 />
                 <path
                     d="M215.48,300h84.52v-84.52c-16.49,37.73-46.8,68.03-84.52,84.52Z"
-                    fill="green"
+                    fill="#275133"
                 />
             </svg>
         </div>
@@ -50,7 +50,7 @@ export function OutsideCorners(props: PieceSquareProps) {
 export function CenterDot() {
     return (
         <svg display="block">
-            <circle cx="50%" cy="50%" r="15%" fill="green" />
+            <circle cx="50%" cy="50%" r="15%" fill="#275133" />
         </svg>
     );
 }
