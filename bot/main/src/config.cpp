ac675af9417f8d540739d6c718401757f996817e
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-07 20:31:02 -0500

    chore: rm bot

--- bot/main/src/config.cpp
+++
@@ -1,9 +0,0 @@
-#include <freertos/FreeRTOS.h> // Mandatory first include
-
-#include <hal/adc_types.h>
-
-#include <chessbot/config.h>
-
-namespace chessbot {
-uint32_t configStore[(size_t)ConfigKey::CONFIG_COUNT] = {};
-}; // namespace chessbot
