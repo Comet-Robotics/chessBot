d22ed6b9943c6067ee071ba7a250b42e8488b8ac
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-26 16:44:26 -0500

    feat: impl simulator update message

---
+++ src/common/message/simulator-message.ts
@@ -0,0 +1,32 @@
+import { Packet } from "../../server/utils/tcp-packet";
+import { Message, MessageType } from "./message";
+export type SimulatedRobotLocation = { position: { x: number, y: number }, heading: number }
+
+export type StackFrame = {
+    fileName: string;
+    functionName: string;
+    lineNumber: number;
+    columnNumber: number;
+}
+
+export class SimulatorUpdateMessage extends Message {
+    protected type = MessageType.SIMULATOR_UPDATE;
+
+    constructor(
+        public readonly robotId: string, 
+        public readonly location: SimulatedRobotLocation, public readonly packet: Packet, 
+        public readonly stackTrace?: StackFrame[]
+    ) {
+        super();
+    }
+
+    protected toObj(): object {
+        return {
+            ...super.toObj(),
+            robotId: this.robotId,
+            location: this.location,
+            packet: this.packet,
+            stackTrace: this.stackTrace,
+        };
+    }
+}
